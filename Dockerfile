3198339810faa6642b0403aa954e9ec717bb4ed8
Florent Carrez <florent.carrez@epita.fr>
2022-08-23 18:30:15 +0200

    making Dockerfile and its associated script.sh

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,20 @@ FROM mongo
 
 WORKDIR /home/
 
+RUN apt-get update -y
+RUN apt-get install -y curl
+
+RUN curl -O -L "https://golang.org/dl/go1.19.linux-amd64.tar.gz"
+
+RUN tar -xf "go1.19.linux-amd64.tar.gz"
+
+RUN mv -v go /usr/local
+
+COPY *go* /home/
+
 COPY ./script.sh .
+
+EXPOSE 8080
+
+CMD ["sh", "script.sh"]
 
--- script.sh
+++ script.sh
@@ -1,3 +1,17 @@
 #! /bin/sh
 
-echo "i will run the needed command"
+nohup sh -c mongod --dbpath /test > mongoLog &
+
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
+go run main.go > serverLog
+
+echo "\n========================================"
+echo "\nMongo Log\n"
+cat mongoLog
+echo "=========================================="
+
+echo "\nserver Log\n"
+cat serverLog
+echo "=========================================="
